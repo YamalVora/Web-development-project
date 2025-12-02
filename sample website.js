267caecfe1bdce245360bf88207a36aea6b32193
YamalVora <yamalvora2006@gmail.com>
2025-12-02 12:50:22 +0530

    Add files via upload

---
+++ sample website.js
@@ -0,0 +1,14 @@
+let h1id = document.getElementById("MYID1");
+// console.log(h1id);      //will print code of ID
+// console.dir(h1id);      //will print directory of ID
+let firstel =document.querySelector("#MYID1");       //will return the first element  
+let secondtel =document.querySelectorAll("#MYID1");  
+
+
+
+console.dir(firstel);
+console.dir(secondtel);
+let id1=document.getElementById("h1id");
+// console.log(id1);
+let div=document.querySelectorAll(".container");
+console.dir(div);
